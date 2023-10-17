49dfec130f28f8126651ca35bcbdac6bebd3f4b4
Imanda03 <asis03ktm@gmail.com>
2023-10-17 16:03:14 +0545

    jwt practice

---
+++ JwtAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using JwtAuthentication.Controllers.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginModel user)
+    {
+        if (user is null)
+        {
+            return BadRequest("Invalid client request");
+        }
+        if (user.Username == "admin" && user.Password == "admin")
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, user.Username),
+        new Claim(ClaimTypes.Role, "Manager")
+    };
+
+            var tokeOptions = new JwtSecurityToken(
+                issuer: "https://localhost:7051",
+                audience: "https://localhost:7051",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: signinCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            return Ok(new AuthenticatedResponse { Token = tokenString });
+        }
+        return Unauthorized();
+    }
+}
---
+++ JwtAuthentication/Controllers/Model/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿namespace JwtAuthentication.Controllers.Model
+{
+    public class LoginModel
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+
+    public class AuthenticatedResponse
+    {
+        public string? Token { get; set;}
+    }
+}
